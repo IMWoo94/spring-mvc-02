3c85465ec64d9eb42aae1f15492b48547a579eb9
IMWoo94 <leemeawoo94@gmail.com>
2023-09-25 21:11:27 +0900

    spring mvc part2 multi checkbox 2023.09.25

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,6 +1,8 @@
 package hello.itemservice.web.form;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -47,6 +49,8 @@ public class FormItemController {
 	@PostMapping("/add")
 	public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
 		log.info("item.open={}", item.getOpen());
+		log.info("item.regions={}", item.getRegions());
+
 		Item savedItem = itemRepository.save(item);
 		redirectAttributes.addAttribute("itemId", savedItem.getId());
 		redirectAttributes.addAttribute("status", true);
@@ -64,6 +68,15 @@ public class FormItemController {
 	public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
 		itemRepository.update(itemId, item);
 		return "redirect:/form/items/{itemId}";
+	}
+
+	@ModelAttribute("regions")
+	public Map<String, String> regions() {
+		Map<String, String> regions = new LinkedHashMap<>();
+		regions.put("SEOUL", "서울");
+		regions.put("BUSAN", "부산");
+		regions.put("JEJU", "제주");
+		return regions;
 	}
 
 }
