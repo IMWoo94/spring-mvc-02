164e115ce6afb65cfefb00e3ff3300af21cee089
IMWoo94 <leemeawoo94@gmail.com>
2023-09-27 18:10:01 +0900

    spring mvc part2 bean validation saveform and updateform use 2023.09.27

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,26 +1,20 @@
 package hello.itemservice.domain.item;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.Range;
-
 import lombok.Data;
 
 @Data
 // @ScriptAssert(lang = "javascript", script = "_this.price * _this.qunantity >= 10000", message = "총합이 10000원 넘게 해주세요.")
 public class Item {
 
-	@NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
+	// @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
 	private Long id;
-	@NotBlank(message = "공백일 수 없습니다.", groups = {SaveCheck.class, UpdateCheck.class})
+	// @NotBlank(message = "공백일 수 없습니다.", groups = {SaveCheck.class, UpdateCheck.class})
 	private String itemName;
-	@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-	@Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+	// @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+	// @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
 	private Integer price;
-	@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-	@Max(value = 9999, groups = SaveCheck.class) // 수정 요구사항 추가
+	// @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+	// @Max(value = 9999, groups = SaveCheck.class) // 수정 요구사항 추가
 	private Integer quantity;
 
 	public Item() {
