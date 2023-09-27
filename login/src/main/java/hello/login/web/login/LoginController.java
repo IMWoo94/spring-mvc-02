6590124603b22ae330e4a80961fbc775653e00dd
IMWoo94 <leemeawoo94@gmail.com>
2023-09-27 20:28:07 +0900

    spring mvc part2 login state cookie use 2023.09.27

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -1,5 +1,7 @@
 package hello.login.web.login;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -26,7 +28,10 @@ public class LoginController {
 	}
 
 	@PostMapping("/login")
-	public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+	public String login(@Valid @ModelAttribute LoginForm form
+		, BindingResult bindingResult
+		, HttpServletResponse response
+	) {
 		if (bindingResult.hasErrors()) {
 			return "login/loginForm";
 		}
@@ -41,6 +46,11 @@ public class LoginController {
 		}
 
 		// 로그인 성공 처리 todo
+
+		// 쿠키에 시간 정보를 주지 않으면 세션 쿠키 ( 브라우저 종료 시 모두 종료 )
+		Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+		response.addCookie(idCookie);
+
 		return "redirect:/";
 	}
 }
