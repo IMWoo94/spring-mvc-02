36154c305885691e4f738a9851c885124e629f34
IMWoo94 <leemeawoo94@gmail.com>
2023-09-28 22:34:28 +0900

    spring mvc part2 servlet exception error filter default DispatcherType.REQUEST and setDispatcherTypes() use 2023.09.28

--- exception/src/main/java/hello/exception/ExceptionApplication.java
+++ exception/src/main/java/hello/exception/ExceptionApplication.java
@@ -2,12 +2,8 @@ package hello.exception;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-import hello.WebConfig;
 
 @SpringBootApplication
-@Import(WebConfig.class)
 public class ExceptionApplication {
 
 	public static void main(String[] args) {
