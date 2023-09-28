8e2b392f2d7d59c916ddc0358a29a148a798075d
IMWoo94 <leemeawoo94@gmail.com>
2023-09-28 22:22:36 +0900

    spring mvc part2 servlet Exception internal re call check intercpetor test add 2023.09.28

--- exception/src/main/java/hello/exception/ExceptionApplication.java
+++ exception/src/main/java/hello/exception/ExceptionApplication.java
@@ -2,8 +2,12 @@ package hello.exception;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import hello.WebConfig;
 
 @SpringBootApplication
+@Import(WebConfig.class)
 public class ExceptionApplication {
 
 	public static void main(String[] args) {
