2c334a8cbadcec18c863892d9445d424b31d92fc
IMWoo94 <leemeawoo94@gmail.com>
2023-10-01 15:45:01 +0900

    spring mvc part2 formatter apply 2023.10.01

--- typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,19 +4,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
-import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
-		registry.addConverter(new StringToIntegerConverter());
-		registry.addConverter(new IntegerToStringConverter());
+		// 주석처리 우선순위 -> 컨버터와 포맷터 의 변환 타입이 동일한 경우 포맷터보다 컨버터가 적용이 된다.
+		// registry.addConverter(new StringToIntegerConverter());
+		// registry.addConverter(new IntegerToStringConverter());
 		registry.addConverter(new StringToIpPortConverter());
 		registry.addConverter(new IpPortToStringConverter());
+
+		// 추가
+		registry.addFormatter(new MyNumberFormatter());
 	}
 }
